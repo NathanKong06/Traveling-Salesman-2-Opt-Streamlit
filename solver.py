47b76fea9750c6a9ed75d063a9f20ed36edc1bbc
NathanKong06 <81611522+NathanKong06@users.noreply.github.com>
2025-12-24 21:28:39 -0800

    Change to max restarts

--- solver.py
+++ solver.py
@@ -2,26 +2,28 @@ from tour import create_random_tour
 from geometry import tour_length
 from two_opt import try_two_opt
 
-def tsp(points, max_iterations):
-    tour = create_random_tour(len(points))
-    length = tour_length(tour, points)
+def tsp(points, max_restarts):
+    best_length = float("inf")
 
-    best_tour = tour[:]
-    best_length = length
+    for restart in range(1, max_restarts + 1):
+        tour = create_random_tour(len(points))
+        length = tour_length(tour, points)
 
-    yield best_tour, best_length
+        current_best_length = length
+        yield tour, current_best_length, restart
 
-    for _ in range(max_iterations):
-        result = try_two_opt(tour, points)
+        improved = True
+        while improved:
+            improved = False
+            result = try_two_opt(tour, points)
 
-        if result is None:
-            tour = create_random_tour(len(points))
-            length = tour_length(tour, points)
-        else:
-            tour, length = result
+            if result is not None:
+                new_tour, new_length = result
+                if new_length < length:
+                    tour, length = new_tour, new_length
+                    improved = True
 
-        if length < best_length:
-            best_tour = tour[:]
-            best_length = length
+                    yield tour, length, restart
 
-        yield best_tour, best_length
+                    if length < best_length:
+                        best_length = length
